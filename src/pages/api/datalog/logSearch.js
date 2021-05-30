65fe7266fccbeb31d66350d5c7bfaaf8108ec740
BobTabrizi <btabrizi@ucsd.edu>
2021-05-29 17:08:33 -0700

    Fixed Imports

--- src/pages/api/datalog/logSearch.js
+++ src/pages/api/datalog/logSearch.js
@@ -1,5 +1,4 @@
-import { connectToDatabase } from "../../../util/mongodb";
-
+import { connectToDatabase } from "../../../../util/mongodb";
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
   const data = await db.collection("testCollection").findOneAndUpdate(
