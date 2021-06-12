c82e0023d81da701c2c85c6bd5ea45a82ec62b9f
BobTabrizi <btabrizi@ucsd.edu>
2021-06-11 23:58:58 -0700

    Fixed dependencies

--- src/BackendFunctions/popLists.js
+++ src/BackendFunctions/popLists.js
@@ -1,25 +1,6 @@
-//import Data from "../../Data/Data.json";
-//import Data from "../../Data/ExternalData.json";
-
 export default function popLists() {
   const getLists = async () => {
     fetch(`${process.env.NEXT_PUBLIC_PROD_URL}/api/refreshLists`);
   };
-
   return getLists();
-
-  /*
-  const getLists = async () => {
-    //  for (var key in Data) {
-    for (let i = 0; i < Data.length; i++) {
-      const id = Data[i].countryID;
-      console.log(id);
-      const data = await fetch(
-        `${process.env.NEXT_PUBLIC_PROD_URL}/api/${id}/tempPopLists?Number=${i}`
-      );
-    }
-  };
-
-  return getLists();
-  */
 }
